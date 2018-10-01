735d94cb74713ead728ab08462797e0217363b8a
Alex Foglia <alex.f1793@gmail.com>
2018-10-01 15:11:30 +0200

    FINITO SPERO

--- Routines/Linear Multistep Methods/comparelmf.m
+++ Routines/Linear Multistep Methods/comparelmf.m
@@ -1,17 +1,15 @@
 fun = @(t,y) 10*y-2*y^2;
 FUN = @(t) 5/(1+4*exp(-10*t));
-intval = linspace(0,2.5,2500);
+intval = linspace(0,2.5,125/2);
 t0 = 0;
 tf = 2.5;
 h = intval(2)-intval(1);
+N = length(intval);
 real_values =zeros(1,length(intval));
 for i=1:length(intval)
     real_values(i)=FUN(intval(i));
 end
 y0 = 1;
-y1 = real_values(1);
-midpoint_ai = [-1; 0; 1];
-midpoint_bi = [0; 2;  0];
 euler_ai = [-1;1];
 euler_bi = [1;0];
 [euler,ti]=linear_multistep(fun,t0,y0,h,tf,euler_ai,euler_bi);
--- Routines/Linear Multistep Methods/linear_multistep.m
+++ Routines/Linear Multistep Methods/linear_multistep.m
@@ -15,27 +15,57 @@ function [y,t] = linear_multistep(f,t0,y0,h,tf,ai,bi)
     t = linspace(t0,t0+h*n,n+1)';
     y = zeros(n+1,1);
     fy = zeros(s1,1);
+    F0 = numjacobian(f,t0,y0);
+    M = inv(eye(s1) -  h*(bi(s1)'*F0));
     y(1)=y0;
-    for k=0:n-s
+    for k=0:n-s1
         G = @(x) ai(s)*x + (ai(1:s1)'*y(k+1:s1+k)) - ...
             h*(bi(1:s1)'*fy) - h*bi(s)*f(t(s+k),x);
-        M = inv(eye(s1) -  h*(bi(1:s1)'*fy));
         y(s+k) = one_step(y(s+k-1),G,M);
         fy(1:s1-1)=fy(2:s1);
         fy(s1)=f(t(s+k),y(s+k));
     end
 end
 
-function [y] = one_step(yn,G,M,tol)
+function [f0] = numjacobian( fun, t0, y0 )
+%
+%  NUMERICAL JACOBIAN  (slightly adapted from the code BiM/BiMD).
+%  2015.02.12
+%
+m  = length(y0);
+Jf = zeros(m);
+f0 = reshape( feval( fun, t0, y0 ), m, 1 );
+dd = eps^(1/3);
+for i = 1:m
+  ysafe = y0(i);
+  delt  = sqrt( eps*max(dd,abs(ysafe)) );
+  y0(i) = ysafe +delt;
+  f1    = feval( fun, t0, y0 );
+  Jf(:,i) = ( f1(:) -f0 )/delt;
+  y0(i) = ysafe;
+end
+if nargout>1, f0 = f0.'; end
+return
+end
+
+
+function [y] = one_step(yn,G,M,tol,maxit)
     if nargin == 3
         tol = 1.e-5;
+        maxit = 1000;
+    elseif nargin == 4
+        maxit = 1000;
     end
     y = yn;
     scal = norm(y)+1;
     Delta = -M*G(y);
-    while norm(Delta)/scal > tol
+    it = 0;
+    while norm(Delta)/scal > tol && it < maxit
         Delta = -M*G(y);
         y = y + Delta;
+        it = it+1;
     end
-        
+    if it == maxit
+        warning('Reached max iteration\n');
+    end
 end
