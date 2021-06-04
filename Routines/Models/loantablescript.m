676492116319380efa3c877f347e224748859d3a
Alex Foglia <alex.f1793@gmail.com>
2021-06-04 10:51:15 +0200

    HO USATO ROBA DI GIGI PER LA VITA VERA INCREYBLE DOPO LA PARTIDA ROMA A SUSSIESSO DOPO LA PARTIDA FIORENTINA TANTISSIMA PARTITA CHI NON POSSIAMO NOI UN PO DE COLPA A LUI A LORO Y LORO A NOI POI FASER QUALCOSA NO??

--- Routines/Models/loantablescript.m
+++ Routines/Models/loantablescript.m
@@ -1,6 +1,6 @@
-Capital = 100000;
-Interest = 0.05;
-N = 10;
+Capital = 70000;
+Interest = 0.002;
+N = 25*12;
 Import = Capital*(Interest)/(1-(1+Interest)^-N);
 [resDebt,intQuote,capQuote] = loantable(Capital,Interest,Import,N);
 table = zeros(N+1,3);
@@ -11,3 +11,23 @@ for j=1:N+1
     table(j,2)=intQuote(j);
     table(j,3)=capQuote(j);
 end
+
+x = linspace(0, 25*12, 1 + 25*12);
+figure();
+plot(x, table(:, 2), 'DisplayName', 'Interest quote')
+hold on;
+plot(x, table(:, 3), 'DisplayName', 'Capital quote');
+plot(x, table(:, 2) + table(:, 3), 'DisplayName', 'Installment');
+
+legend(gca, 'show');
+
+figure();
+yy = (table(:, 2) + table(:, 3))
+y = zeros(1, N + 1);
+for i = 1:length(y)
+  y(i) = x(i) * yy(i);
+endfor
+plot(x, table(:, 1), 'DisplayName', 'Residual debt');
+hold on;
+plot(x, y, 'DisplayName', 'Cumulative spending');
+legend(gca, 'show');
